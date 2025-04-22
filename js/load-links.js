dd82bd3ff3ef07a8a6616a156b33744d5a822412
parkerjb <parkerjb@gmail.com>
2025-04-22 11:45:18 -0500

    Fixes incorrectly commited debug code

--- js/load-links.js
+++ js/load-links.js
@@ -1,44 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     // Fetch the JSON data
-    var links = [
-    {
-      "name": "bagsync",
-      "url": "https://www.curseforge.com/wow/addons/bagsync/download"
-    }, {
-      "name": "battle-pet-battle-ui-tweaks",
-      "url": "https://www.curseforge.com/wow/addons/battle-pet-battle-ui-tweaks/download"
-    }, {
-      "name": "big-wigs",
-      "url": "https://www.curseforge.com/wow/addons/big-wigs/download"
-    }, {
-      "name": "little-wigs",
-      "url": "https://www.curseforge.com/wow/addons/little-wigs/download"
-    }, {
-      "name": "bug-grabber",
-      "url": "https://www.curseforge.com/wow/addons/bug-grabber/download"
-    }, {
-      "name": "bugsack",
-      "url": "https://www.curseforge.com/wow/addons/bugsack/download"
-    }, {
-      "name": "details",
-      "url": "https://www.curseforge.com/wow/addons/details/download"
-    }];
-    displayLinks(links);
-    // fetch('links.json')
-    //     .then(response => {
-    //         if (!response.ok) {
-    //             throw new Error('Network response was not ok');
-    //         }
-    //         return response.json();
-    //     })
-    //     .then(data => {
-    //         displayLinks(data.links);
-    //     })
-    //     .catch(error => {
-    //         console.error('Error loading links:', error);
-    //         document.getElementById('links-container').innerHTML = 
-    //             '<li>Sorry, could not load links. Please try again later.</li>';
-    //     });
+    fetch('links.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json();
+        })
+        .then(data => {
+            displayLinks(data.links);
+        })
+        .catch(error => {
+            console.error('Error loading links:', error);
+            document.getElementById('links-container').innerHTML = 
+                '<li>Sorry, could not load links. Please try again later.</li>';
+        });
 });
 
 function displayLinks(links) {
